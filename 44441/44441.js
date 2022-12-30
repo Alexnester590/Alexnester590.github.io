c936089656a1044a1713c4b95cce9fbf1a5fa6b0
Alexnester590 <85796742+Alexnester590@users.noreply.github.com>
2022-12-30 07:37:47 +0300

    Add files via upload

---
+++ 44441/44441.js
@@ -0,0 +1,27 @@
+$(document).ready(async function(){
+    async function saveNewPlayer() {
+        //Create your Parse Object
+        const soccerPlayers = new Parse.Object("SoccerPlayers");
+
+        //Define its attributes
+        soccerPlayers.set("playerName", "A. Wed");
+        soccerPlayers.set("yearOfBirth", 1997);
+        soccerPlayers.set("emailContact", "a.wed@email.io");
+        soccerPlayers.set("attributes", ["fast","good conditioning"])
+        try{
+            //Save the Object
+            let result = await soccerPlayers.save()
+            alert('New object created with objectId: ' + result.id);
+        }catch(error){
+            alert('Failed to create new object, with error code: ' + error.message);
+        }
+    } 
+});
+
+
+
+
+
+
+//end doc ready
+
